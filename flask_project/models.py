6be37d179d1a072602dc46a8fcb15f176c1d88cf
Ashwanth Prithish <ashwanthprithishcse@gmail.com>
2024-10-08 10:11:48 +0530

    with comments(old code) in models.py

--- flask_project/models.py
+++ flask_project/models.py
@@ -1,29 +1,43 @@
-from flask_project import db, login_manager
-from datetime import datetime
-from flask_login import UserMixin
+# from flask_project import db, login_manager
+# from datetime import datetime
+# from flask_login import UserMixin
 
-@login_manager.user_loader
-def load_user(user_id):
-    if int(user_id) >= 10000:
-        return Service_Professional.query.get(int(user_id))
-    else:
-        return Customer.query.get(int(user_id))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     if int(user_id) >= 10000:
+#         return Service_Professional.query.get(int(user_id))
+#     else:
+#         return Customer.query.get(int(user_id))
 
-class Customer(db.Model, UserMixin):
-    __tablename__ = 'customer'  
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    address = db.Column(db.String(400), nullable=False)
-    contact = db.Column(db.String(15), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
+# class Customer(db.Model, UserMixin):
+#     __tablename__ = 'customer'  
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     address = db.Column(db.String(400), nullable=False)
+#     contact = db.Column(db.String(15), unique=True, nullable=False)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+#     password = db.Column(db.String(60), nullable=False)
 
-    # Relationship to Service_Request
-    service_requests = db.relationship('Service_Request', backref='customer', lazy=True)
+#     # Relationship to Service_Request
+#     service_requests = db.relationship('Service_Request', backref='customer', lazy=True)
 
-    def __repr__(self):
-        return f"Customer('{self.username}', '{self.email}', '{self.image_file}')"
+#     def __repr__(self):
+#         return f"Customer('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+# # class Service(db.Model):
+# #     __tablename__ = 'service'
+# #     id = db.Column(db.Integer, primary_key=True)
+# #     name = db.Column(db.String(100), unique=True, nullable=False)
+# #     price = db.Column(db.String(100), nullable=False)
+# #     description = db.Column(db.Text, nullable=False)
+
+# #     # Relationships
+# #     service_requests = db.relationship('Service_Request', backref='service', lazy=True)
+
+# #     def __repr__(self):
+# #         return f"Service('{self.name}', '{self.description}')"
 
 
 # class Service(db.Model):
@@ -33,79 +47,65 @@ class Customer(db.Model, UserMixin):
 #     price = db.Column(db.String(100), nullable=False)
 #     description = db.Column(db.Text, nullable=False)
 
-#     # Relationships
-#     service_requests = db.relationship('Service_Request', backref='service', lazy=True)
+#     # Explicit join condition to relate through Service_Professional
+#     service_requests = db.relationship(
+#         'Service_Request',
+#         primaryjoin="Service.id == Service_Professional.service_id",
+#         secondaryjoin="Service_Professional.id == Service_Request.service_professional_id",
+#         secondary="service_professional",  # Join via the 'service_professional' table
+#         backref='service', 
+#         lazy=True
+#     )
 
 #     def __repr__(self):
 #         return f"Service('{self.name}', '{self.description}')"
 
 
-class Service(db.Model):
-    __tablename__ = 'service'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    price = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=False)
+# class Service_Professional(db.Model, UserMixin):
+#     __tablename__ = 'service_professional'
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     description = db.Column(db.String(400), nullable=False)
+#     experience = db.Column(db.String(400), nullable=False)
+#     date_created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+#     password = db.Column(db.String(60), nullable=False)
 
-    # Explicit join condition to relate through Service_Professional
-    service_requests = db.relationship(
-        'Service_Request',
-        primaryjoin="Service.id == Service_Professional.service_id",
-        secondaryjoin="Service_Professional.id == Service_Request.service_professional_id",
-        secondary="service_professional",  # Join via the 'service_professional' table
-        backref='service', 
-        lazy=True
-    )
+#     # Foreign key to relate to Service
+#     service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+#     service_requests = db.relationship('Service_Request', backref='service_professional', lazy=True)
 
-    def __repr__(self):
-        return f"Service('{self.name}', '{self.description}')"
+#     def __repr__(self):
+#         return f"Service Professional('{self.username}', '{self.email}')"
 
 
-class Service_Professional(db.Model, UserMixin):
-    __tablename__ = 'service_professional'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    description = db.Column(db.String(400), nullable=False)
-    experience = db.Column(db.String(400), nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
+# class Service_Request(db.Model):
+#     __tablename__ = 'service_request'
+#     id = db.Column(db.Integer, primary_key=True)
+#     date_of_request = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#     date_of_completion = db.Column(db.DateTime, nullable=False)
+#     service_status = db.Column(db.String(100), nullable=False)
 
-    # Foreign key to relate to Service
-    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
-    service_requests = db.relationship('Service_Request', backref='service_professional', lazy=True)
+#     # Foreign keys
+#     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+#     service_professional_id = db.Column(db.Integer, db.ForeignKey('service_professional.id'), nullable=True)
+#     remark_id = db.Column(db.Integer, db.ForeignKey('remarks.id'), nullable=True)
 
-    def __repr__(self):
-        return f"Service Professional('{self.username}', '{self.email}')"
+#     def __repr__(self):
+#         return f"Service_Request('{self.date_of_request}', '{self.date_of_completion}')"
 
 
-class Service_Request(db.Model):
-    __tablename__ = 'service_request'
-    id = db.Column(db.Integer, primary_key=True)
-    date_of_request = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    date_of_completion = db.Column(db.DateTime, nullable=False)
-    service_status = db.Column(db.String(100), nullable=False)
+# class Remarks(db.Model):
+#     __tablename__ = 'remarks'
+#     id = db.Column(db.Integer, primary_key=True)
+#     remarks = db.Column(db.String(100), nullable=False)
 
-    # Foreign keys
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
-    service_professional_id = db.Column(db.Integer, db.ForeignKey('service_professional.id'), nullable=True)
-    remark_id = db.Column(db.Integer, db.ForeignKey('remarks.id'), nullable=True)
+#     # Foreign key
+#     service_request_id = db.Column(db.Integer, db.ForeignKey('service_request.id'), nullable=False)
 
-    def __repr__(self):
-        return f"Service_Request('{self.date_of_request}', '{self.date_of_completion}')"
-
-
-class Remarks(db.Model):
-    __tablename__ = 'remarks'
-    id = db.Column(db.Integer, primary_key=True)
-    remarks = db.Column(db.String(100), nullable=False)
-
-    # Foreign key
-    service_request_id = db.Column(db.Integer, db.ForeignKey('service_request.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Remark('{self.service_request_id}', '{self.remarks}')"
+#     def __repr__(self):
+#         return f"Remark('{self.service_request_id}', '{self.remarks}')"
 
 
 from flask_project import db, login_manager
